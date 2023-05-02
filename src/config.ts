eb2ee6ec50472da75a8ef30533e3569be70af288
Hugo <61784326+Hugo-Farias@users.noreply.github.com>
2023-05-02 10:27:42 -0300

    Updated LinkedIn URL

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ export const LAST_NAME = "farias";
 export const GITHUB_GRAPHQL_URL = "https://api.github.com/graphql";
 export const GITHUB_URL = "https://github.com/Hugo-Farias";
 export const LINKEDIN_URL =
-  "https://www.linkedin.com/in/hugo-farias-668274238/";
+  "https://www.linkedin.com/in/hugofariasdev";
 export const FRONTENDMENTOR_URL =
   "https://www.frontendmentor.io/profile/Hugo-Farias";
