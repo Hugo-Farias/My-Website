7f0523d508b95fb1aeae7e5bebaa9807dc022b58
Hugo-Farias <hg80rod@gmail.com>
2023-07-03 14:59:45 -0300

    added animation to modal

--- src/components/home/CVSection.tsx
+++ src/components/home/CVSection.tsx
@@ -1,17 +1,21 @@
 import "./CVSection.scss";
+import { useState } from "react";
 import Btn from "../common/Btn";
+import Modal from "../common/Modal";
 
 interface prop {
   status: boolean;
 }
 
 const CVSection = function ({ status }: prop) {
+  const [cvModal, setCvModal] = useState(false);
   const fileName = "hugo-rodrigues-farias_";
 
   return (
     <div className={`cv-section ${status}`}>
-      <Btn onClick={() => console.log(fileName + "en")}>En</Btn>
-      <Btn onClick={() => console.log(fileName + "pt")}>Pt-Br</Btn>
+      <Btn onClick={() => setCvModal(true)}>En</Btn>
+      <Btn onClick={() => setCvModal(true)}>Pt-Br</Btn>
+      <Modal close={() => setCvModal(false)}>whatever</Modal>
     </div>
   );
 };
