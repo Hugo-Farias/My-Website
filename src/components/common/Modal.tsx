dd97474505cbbfe9b17a6027e592e632a8516650
Hugo-Farias <hg80rod@gmail.com>
2023-07-05 15:34:26 -0300

    pdf working but possibly unresponsive

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -4,17 +4,22 @@ import React from "react";
 interface prop {
   close: () => void;
   children: React.ReactNode;
+  hideCloseBtn?: boolean;
 }
 
 const Modal = function (prop: prop) {
-  const { close, children } = prop;
+  const { close, children, hideCloseBtn } = prop;
 
   return (
     <>
       <div className="modal">
-        <span className="close-button" onClick={close}>
-          &times;
-        </span>
+        {!hideCloseBtn ? (
+          <span className="close-button" onClick={close}>
+            &times;
+          </span>
+        ) : (
+          ""
+        )}
         {children}
       </div>
       <div className="modal-backdrop" onClick={close} />
--- src/components/home/CVSection.tsx
+++ src/components/home/CVSection.tsx
@@ -3,22 +3,32 @@ import { useState } from "react";
 import Btn from "../common/Btn";
 import { CSSTransition } from "react-transition-group";
 import Modal from "../common/Modal";
+import cvEn from "../../assets/docs/hugo-rodrigues-farias_en.pdf";
+import cvPt from "../../assets/docs/hugo-rodrigues-farias_pt.pdf";
 
 interface prop {
   status: boolean;
 }
 
 const CVSection = function ({ status }: prop) {
-  const [cvModal, setCvModal] = useState(false);
-  const fileName = "hugo-rodrigues-farias_";
+  const [cvModal, setCvModal] = useState<boolean>(false);
+  const [cvDoc, setCvDoc] = useState<string>(cvEn);
+
+  const handleCv = function (lang: "en" | "pt") {
+    setCvModal(true);
+
+    if (lang === "en") return setCvDoc(cvEn);
+
+    setCvDoc(cvPt);
+  };
 
   return (
     <div className={`cv-section ${status}`}>
-      <Btn onClick={() => setCvModal(true)}>En</Btn>
-      <Btn onClick={() => setCvModal(true)}>Pt-Br</Btn>
+      <Btn onClick={handleCv.bind(null, "en")}>En</Btn>
+      <Btn onClick={handleCv.bind(null, "pt")}>Pt-Br</Btn>
       <CSSTransition in={cvModal} timeout={300} unmountOnExit>
         <Modal close={() => setCvModal(false)}>
-          <div className="cv-content"></div>
+          <embed src={cvDoc} type="application/pdf" />
         </Modal>
       </CSSTransition>
     </div>
